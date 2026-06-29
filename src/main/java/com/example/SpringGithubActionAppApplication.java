d294adcd30d8267edaeb6cee583c2b0e1bcf9601
sub1n17 <sub1n17@naver.com>
2026-06-29 15:21:54 +0900

    commit 0.0.1

---
+++ src/main/java/com/example/SpringGithubActionAppApplication.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringGithubActionAppApplication {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("/")
+    public String index() {
+        return appName + "-" + appVersion;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGithubActionAppApplication.class, args);
+    }
+
+}
